6e45262312010414440dbbcb1a8fbf8d1bae8b06
Ashamu Oluwatimileyin Ifeoluwa <ifeoluwaoluwatimileyin2018@gmail.com>
2024-04-05 14:14:48 +0100

    firstcommit

---
+++ bank-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom"
+import Signup from './Components/Signup'
+
+function App() {
+
+  return (
+    <>
+    < Signup />
+    < Routes>
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
