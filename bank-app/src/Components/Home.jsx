9b6b76a9be55dc2e7ce8fe0ae7431ff28ae47164
Ashamu Oluwatimileyin Ifeoluwa <ifeoluwaoluwatimileyin2018@gmail.com>
2024-04-07 21:47:38 +0100

    parttwo

--- bank-app/src/Components/Home.jsx
+++ bank-app/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Wallet from "../assets/Images/Wallet.gif"
 import './Homes.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -8,15 +9,15 @@ const Home = () => {
             <div className='Old'>
                 <div className='start'>
                     <img src={Wallet} alt="" />
-                    <div>
+                    <div className='Get'>
                         <h1>Let's Get Started</h1>
-                        <p>it is a long established fact that a be distracted by the readable a page when looking at its</p>
+                        <p>it is a long established fact that a be <br /> distracted by the readable a page when looking at its</p>
                     </div>
                 </div>
 
-                <div className='create'>
-                    <button>Create account</button>
-                    <button>Sign in to Continue</button>
+                <div className='account'>
+                    <button  className='create'><Link to="/signup">Create account</Link></button><br />
+                    <button className='sign'><Link to="/sigin">Sign in to Continue</Link></button>
                 </div>
             </div>
         </>
